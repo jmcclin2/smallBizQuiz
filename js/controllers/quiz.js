96810939e50351d64bc305a8481249f88c30e1f4
Mcclintock <JMcClintock@ad.harman.com>
2018-08-07 15:36:53 -0700

    Quiz questions loading OK - next implment answer selection and results

--- js/controllers/quiz.js
+++ js/controllers/quiz.js
@@ -12,6 +12,39 @@
 
         vm.quizMetrics = quizMetrics;
         vm.dataService = DataService;
+        vm.activeQuestion = 0;
+
+        var numQuestionsAnswered = 0;
+
+        vm.setActiveQuestion = setActiveQuestion;
+
+        function setActiveQuestion() {
+            var breakOut = false;
+            var quizLength = DataService.quizQuestions.length - 1;
+        
+            while (!breakOut) {
+                vm.activeQuestion = vm.activeQuestion < quizLength?++vm.activeQuestion:0;
+                if (DataService.quizQuestions[vm.activeQuestion].selected === null) {
+                    breakOut = true;
+                }
+            }
+        }
+ 
+        vm.questionAnswered = questionAnswered;
+
+        function questionAnswered() {
+ 
+            var quizLength = DataService.quizQuestions.length;
+
+            if (DataService.quizQuestions[vm.activeQuestion].selected !== null) {
+                numQuestionsAnswered++;
+
+                if (numQuestionsAnswered >= quizLength) {
+                    //Finalise the quiz
+                }
+            }
+            vm.setActiveQuestion(); 
+        }
     }
  
 })();
